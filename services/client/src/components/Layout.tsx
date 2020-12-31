87b0d0ad2161f85c66f15ac1aef2323381d1f107
richardnguyen99 <richard.ng0616@gmail.com>
2020-12-30 17:43:16 -0800

    Add navbar

--- services/client/src/components/Layout.tsx
+++ services/client/src/components/Layout.tsx
@@ -1,7 +1,40 @@
+/* eslint-disable react/button-has-type */
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Layout: React.FC = ({ children }) => {
-  return <main className="page">{children}</main>;
+  return (
+    <>
+      <div className="tdp-navbar-content shadow">
+        <div className="tdp-navbar">
+          <div className="tdp-navbar__left">
+            <p>Todo+</p>
+          </div>
+          <div className="tdp-navbar__center">
+            <div className="tdp-navbar__group">
+              <button className="tdp-navbar__group__item">About</button>
+            </div>
+            <div className="tdp-navbar__group">
+              <button className="tdp-navbar__group__item">Github</button>
+            </div>
+          </div>
+          <div className="tdp-navbar__right">
+            <button className="tdp-button tdp-button--primary tdp-button--border">
+              <div className="tdp-button__content">
+                <Link to="/register">Register</Link>
+              </div>
+            </button>
+            <button className="tdp-button tdp-button--primary tdp-button--default">
+              <div className="tdp-button__content">
+                <Link to="/login">Login</Link>
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+      <main className="page">{children}</main>
+    </>
+  );
 };
 
 export default Layout;
